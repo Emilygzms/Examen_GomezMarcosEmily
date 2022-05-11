5322dd79c90e6ff2b32ea9d1511a8b6495a74c4d
Alonso Ponce <alonsosu2907@gmail.com>
2022-05-11 10:57:57 -0500

    Archivos creados por ALONSO PONCE SUSANIBAR

---
+++ Preg01.java
@@ -0,0 +1,18 @@
+package ejercicios.cap1.lp3;
+
+import java.util.Scanner;
+
+public class EJERCICIO1 {
+
+    public static void main(String[] args) {
+       
+        Scanner teclado=new Scanner(System.in);
+        System.out.println("Digite N: ");
+        double num=teclado.nextDouble();
+        double sumatoria=0.25-(1/(num+4));
+        System.out.println("Sumatoria: " + sumatoria);
+        
+            
+    }
+    
+}
---
+++ Preg02.java
@@ -0,0 +1,23 @@
+package ejercicios.cap1.lp3;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Datos de entrada:\nN: ");
+        Scanner numero = new Scanner(System.in);
+        int n = numero.nextInt();
+        int l = Integer.toString(n).length();
+        
+        System.out.println("\nDatos de salida: ");
+        
+        for (int i=0; i <l; i++){
+            String num = String.valueOf(Integer.toString(n).charAt(i));
+            System.out.println(num + "\n");
+        }
+        
+    }
+    
+}
